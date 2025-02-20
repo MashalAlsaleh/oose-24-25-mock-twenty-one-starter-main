5e8e8472dee7c82731f34a3c80e5fd553770d918
“MashalAlsaleh” <148557035+Handsome-rascal@users.noreply.github.com>
2025-02-20 18:50:41 +0000

    completed Task Snap - Medium

--- src/test/java/card/game/snap/MockSnapTest.java
+++ src/test/java/card/game/snap/MockSnapTest.java
@@ -10,11 +10,10 @@ import java.util.Scanner;
 import card.entity.Hand;
 import card.entity.Card;
 
-
 public class MockSnapTest {
     Snap snap = new Snap("C3,H3,D3,S3");
     Scanner mockScanner = mock(Scanner.class);
-    
+
     @Test
     void testPlayerPlaysHandScore(){
         Hand hand = new Hand("S5,S4,S3");
@@ -28,18 +27,45 @@ public class MockSnapTest {
     }
 
     @Test
-    void testPlayerPlaysHandDiffFaceCards(){
- 
+    void testPlayerPlaysHandSnapYesMatchingCards(){
+        Hand hand = new Hand("C3,C3");
+        snap.createHumanPlayer("Derek");
+        snap.getUser().addHand(hand);
+        when(mockScanner.nextLine()).thenReturn("Yes");
+        snap.setConsoleScanner(mockScanner);
+        snap.playerPlaysHand(snap.getUser());
+        assertEquals(1, snap.getUser().getScore());
     }
 
     @Test
-    void testPlayerPlaysHandNo(){
-      
+    void testPlayerPlaysHandSnapYesDifferentCards(){
+        Hand hand = new Hand("C3,H4");
+        snap.createHumanPlayer("Derek");
+        snap.getUser().addHand(hand);
+        when(mockScanner.nextLine()).thenReturn("Yes");
+        snap.setConsoleScanner(mockScanner);
+        snap.playerPlaysHand(snap.getUser());
+        assertEquals(-1, snap.getUser().getScore());
     }
 
     @Test
-    void testPlay(){
+    void testPlaySnapYesMatchingCards(){
+        snap = new Snap("C3,C3");
+        snap.createHumanPlayer("Derek");
+        when(mockScanner.nextLine()).thenReturn("Yes");
+        snap.setConsoleScanner(mockScanner);
+        snap.play();
+        assertEquals(1, snap.getUser().getScore());
+    }
 
+    @Test
+    void testPlaySnapYesDifferentCards(){
+        snap = new Snap("C3,H4");
+        snap.createHumanPlayer("Derek");
+        when(mockScanner.nextLine()).thenReturn("Yes");
+        snap.setConsoleScanner(mockScanner);
+        snap.play();
+        assertEquals(-1, snap.getUser().getScore());
     }
 }
 
