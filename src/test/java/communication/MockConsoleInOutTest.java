005467dc261ed8d90ad19395888adafa381700b1
“MashalAlsaleh” <148557035+Handsome-rascal@users.noreply.github.com>
2025-02-20 18:36:17 +0000

    completed Task src/test/java/communication/MockConsoleInOutTest - Easy

--- src/test/java/communication/MockConsoleInOutTest.java
+++ src/test/java/communication/MockConsoleInOutTest.java
@@ -1,12 +1,19 @@
 package communication;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
+
+import card.entity.Card;
+import card.entity.Hand;
+import card.entity.Player;
+import card.entity.PlayerType;
 
 public class MockConsoleInOutTest{
 
@@ -23,7 +30,69 @@ public class MockConsoleInOutTest{
         when(mockScanner.nextLine()).thenReturn("2");
         assertEquals(2, consoleInOut.getInteger());
     }
-
-
     
+    @Test
+    void testGetInputInteger(){
+        when(mockScanner.nextLine()).thenReturn("2");
+        assertEquals(2, consoleInOut.getInputInteger("Enter a number:"));
+    }
+    
+    @Test
+    void testGetInputIntegerSecondTime(){
+        when(mockScanner.nextLine()).thenReturn("Del", "2");
+        assertEquals(2, consoleInOut.getInputInteger("Enter a number:"));
+    }
+    
+    @Test
+    void testGetListIndex(){
+        List<String> options = Arrays.asList("One", "Two");
+        when(mockScanner.nextLine()).thenReturn("1");
+        assertEquals(1, consoleInOut.getListIndex(options));
+    }
+
+    @Test
+    void testGetListIndexSecondTime(){
+        List<String> options = Arrays.asList("One", "Two");
+        when(mockScanner.nextLine()).thenReturn("3", "1");
+        assertEquals(1, consoleInOut.getListIndex(options));
+    }
+    
+    @Test
+    void testGetListIndexWithQuestion(){
+        List<String> options = Arrays.asList("One", "Two");
+        when(mockScanner.nextLine()).thenReturn("1");
+        assertEquals(1, consoleInOut.getListIndex("Select an option:", options));
+    }
+
+    @Test
+    void testGetPlayersCard(){
+        Player player = new Player(PlayerType.USER,"TestPlayer");
+        player.addHand(new Hand("C3,C4,C5"));
+        when(mockScanner.nextLine()).thenReturn("1");
+        assertEquals("C4", consoleInOut.getPlayersCard(player).toString());
+    }
+    
+    @Test
+    void testGetYesOrNoYes(){
+        when(mockScanner.nextLine()).thenReturn("Yes");
+        assertEquals(YesOrNo.YES, consoleInOut.getYesOrNo("Confirm?"));
+    }
+
+    @Test
+    void testGetYesOrNoY(){
+        when(mockScanner.nextLine()).thenReturn("Y");
+        assertEquals(YesOrNo.YES, consoleInOut.getYesOrNo("Confirm?"));
+    }
+
+    @Test
+    void testGetYesOrNoNo(){
+        when(mockScanner.nextLine()).thenReturn("N");
+        assertEquals(YesOrNo.NO, consoleInOut.getYesOrNo("Confirm?"));
+    }
+
+    @Test
+    void testGetYesOrNoOther(){
+        when(mockScanner.nextLine()).thenReturn("");
+        assertEquals(YesOrNo.NO, consoleInOut.getYesOrNo("Confirm?"));
+    }
 }
